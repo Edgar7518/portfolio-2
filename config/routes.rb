a3be6fa08a79f8d71ca3bd616879cb5e1a19c8a0
edgar quenum <quenum.edgar@gmail.com>
2023-03-15 18:29:56 -0400

    suppression doublon routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pages#home"
   resources :projects, only: [:index, :show]
   
   get "about", to: "pages#about"
